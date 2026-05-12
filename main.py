ac1d1f4660c6dd94e597375cddf562c5cf524316
chail <1744315424@qq.com>
2026-05-12 22:55:26 +0800

    完成添加成绩的功能

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+import os
 
 class Menu(object):
     info = ["==== 学生成绩管理与分析系统 ====",
@@ -14,26 +16,62 @@ class Menu(object):
             "请输入功能编号："
             ]
 
-    df_scores = pd.read_csv("/data/scores.csv", header=None, names=['id', 'name', 'chinese', 'math', 'english'])
+    df_scores = pd.read_csv("data/scores.csv", header=None, names=['id', 'name', '语文', '数学', '英语'])
+
+    def __init__(self):
+        self.df_scores.set_index('id', inplace=True)
 
     #展示菜单信息
     def show_info(self):
         for s in self.info:
             print(s)
+        self.transit()
+    #获取选项
+    def get_choice(self):
+        while True:
+            sopt = input()
+            if not sopt:
+                continue
+            opt = int(sopt)
+            if 1<=opt<=8:
+                return opt
+            else:
+                print("输入有误，请重新输入！")
+    #转入功能
+    def transit(self):
+        opt = self.get_choice()
+        if opt == 1:
+            self.add_score()
     #成绩格式校验
-    def check(self):
-        pass
+    def check(self, id, subject, score, name):
+        pattern_id = r'^\d{8}$'
+        if not re.fullmatch(pattern_id, id):
+            return False
+        pattern_score = r'^(\d{1,2}|100)$'
+        if not re.fullmatch(pattern_score, score):
+            return False
+        if id not in self.df_scores.index:
+            self.df_scores.loc[id] = [name, pd.NA, pd.NA, pd.NA]
+            return True
+        if pd.isna(self.df_scores.loc[id, subject]):
+            return True
+        else:
+            return False
+
+
     #添加成绩
     def add_score(self):
         print("输入：学号、姓名、科目（语文/数学/英语）、成绩")
         pid,pname,psubject,pscore = input().split()
-        pscore = int(pscore)
-        if self.check():
-            pass
+        if self.check(pid, psubject, pscore, pname):
+            self.df_scores.loc[pid, psubject] = pscore
+            self.df_scores.to_csv("data/scores.csv")
+            print(f"添加成功！{self.df_scores.loc[pid]}")
         else:
-            print("数据格式有误！")
+            print("数据格式有误或重复添加！")
 
 if __name__ == "__main__":
+
     menu = Menu()
     while True:
         menu.show_info()
